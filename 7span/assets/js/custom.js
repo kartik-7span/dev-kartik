5a2b28fc870723a32d2bd3be00a743794c447888
kartik patel <kartik@7span.com>
2023-01-16 18:55:15 +0530

    7span home page

---
+++ 7span/assets/js/custom.js
@@ -0,0 +1,76 @@
+// Loader Js
+    tl = gsap.timeline({
+        defaults: {
+            duration: 2.0,
+            ease: "expo.inOut"
+        }
+    });
+  tl.to(".slide-1", { width: 0, duration: 2.0 }).to("#loader", { height: 0 ,duration: 3.0});
+
+// Btn Hover effect
+
+$(function() {  
+  $('.btn')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('.btn__bg--animation').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('.btn__bg--animation').css({top:relY, left:relX})
+    });
+});
+
+
+// SVG file to SVG code convert JS Start
+function img2svg() {
+    jQuery('.in-svg').each(function(i, e) {
+        var $img = jQuery(e);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', ' ' + imgClass + ' replaced-svg');
+            }
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+}
+img2svg();
+// SVG file to SVG code convert JS End
+
+const introTitle = new SplitText('.intro__title', {type: "lines", linesClass: "intro-line"});
+function initIntro() {
+    
+    // animate the intro elements into place
+    
+    let tl = gsap.timeline({delay: 1.2});
+    
+    tl.from('.intro-line', {
+        // x: 100,
+        y: 400,
+        ease: 'power4',
+        duration: 3
+    })
+    .from('.intro__txt', {
+        x: -100,
+        opacity: 0,
+        ease: 'power4',
+        duration: 3
+    }, 0.7)
+}
